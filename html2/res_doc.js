823b5c6c400fc6a844ecbd224516043e54c615bd
kimgywls <65155823+kimgywls@users.noreply.github.com>
2020-06-03 23:08:26 +0900

    Update res_doc.js

--- html2/res_doc.js
+++ html2/res_doc.js
@@ -1,5 +1,7 @@
-//2020.06.02 김효진-
-$('.content').ready(function(){
-  var st = $(":input:radio[name=Depart_radio]:checked").val();
-  location.href('res_doc.html?st='+st);
+//2020.06.03 김효진
+$('.content').click(function(){
+  var depart = $(":input:radio[name=Depart_radio]:checked").val();
+  var doctor = $(":input:radio[name=doctor]:checked").val();
+  location.href('res_date.html?depart='+depart+'?doctor='+doctor);
+  //location.href('res_date.html?doctor='+doctor);
 });
