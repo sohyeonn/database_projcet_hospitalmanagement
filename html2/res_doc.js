c879a4ed646f62f25c8a0653bcdb83dc25dbe39c
kimgywls <65155823+kimgywls@users.noreply.github.com>
2020-06-03 23:08:42 +0900

    Update res_doc.js

--- html2/res_doc.js
+++ html2/res_doc.js
@@ -3,5 +3,4 @@ $('.content').click(function(){
   var depart = $(":input:radio[name=Depart_radio]:checked").val();
   var doctor = $(":input:radio[name=doctor]:checked").val();
   location.href('res_date.html?depart='+depart+'?doctor='+doctor);
-  //location.href('res_date.html?doctor='+doctor);
 });
