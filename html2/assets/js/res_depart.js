7e08bae77aaf4cf0f2f0348a7628630de20339c5
kimgywls <65155823+kimgywls@users.noreply.github.com>
2020-06-20 14:32:17 +0900

    Update res_depart.js

--- html2/assets/js/res_depart.js
+++ html2/assets/js/res_depart.js
@@ -1,5 +1,21 @@
 $('.content').click(function(){
   var depart = $(":input:radio[name=Depart_radio]:checked").val();
   //alert(st);
-  location.href('res_doc.html?depart='+depart);
+  var num=getParameter("num", false)
+  console.log(num);
+  // location.href('res_doc.html?depart='+depart);
+  location.href('res_doc.html?num='+num+'&depart='+depart);
 });
+
+
+function getParameter(name, url) {
+  if(!url) url = window.location.href;
+
+  //console.log(url);
+
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+  results = regex.exec(url);
+
+  return results[2];
+}
