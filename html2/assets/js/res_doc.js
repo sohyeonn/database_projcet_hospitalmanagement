253ee2af184db6a4a3c8125ca9796a6b80b0e5bf
kimgywls <65155823+kimgywls@users.noreply.github.com>
2020-06-20 14:33:16 +0900

    Update res_doc.js

--- html2/assets/js/res_doc.js
+++ html2/assets/js/res_doc.js
@@ -1,7 +1,33 @@
 //2020.06.03 김효진
 $('.content').click(function(){
-  var depart = $(":input:radio[name=Depart_radio]:checked").val();
   var doctor = $(":input:radio[name=doctor]:checked").val();
-  location.href('res_date.html?depart='+depart+'?doctor='+doctor);
-  //location.href('res_date.html?doctor='+doctor);
+  var num=getParameter("num", false)
+  var depart=getParameter("depart", false)
+  console.log(num);
+  console.log(doctor);
+  console.log(depart);
+
+  $.ajax({
+    url				: './res_doc_result.php?num='+num+'&depart='+depart+'&doctor='+doctor,
+    data			: {"num": num, "doctor": doctor,"depart": depart},
+    type			: 'GET',
+    success		: function(data){
+    	//alert(data);
+      location.href='res_doc_result.php?num='+num+'&depart='+depart+'&doctor='+doctor
+
+    },
+    error 		: function(xhr,status,error){
+      alert(error);
+    },
+  })
+
 });
+function getParameter(name, url) {
+  if(!url) url = window.location.href;
+
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+  results = regex.exec(url);
+
+  return results[2];
+}
