5d9428b30c2bcbe7812b220e5771c008cf6ed694
kimgywls <65155823+kimgywls@users.noreply.github.com>
2020-06-20 14:30:57 +0900

    Update res_member_result.php

--- html2/res_member_result.php
+++ html2/res_member_result.php
@@ -1,4 +1,5 @@
-<?php
+
+<?
 		$host = 'localhost';
 		$user = 'root';
 		$pw = '0000';
@@ -12,15 +13,13 @@
 		$sql1 = "insert into 환자 values (0, '$name', $id_num, '$phone', null, null, null, null, null, null, null, null)";
 		$result1 = mysqli_query($mysqli,$sql1);
 
-		$sql2 = "select * from 환자 where 이름='$name' and 연락처='$phone'";
+		$sql2 = "select * from 환자 where 이름='$name' and 주민번호='$id_num' and 연락처='$phone'";
 		$result2 = mysqli_query($mysqli,$sql2);
 
 		$row =mysqli_fetch_array($result2);
 		$num=$row["환자번호"];
 
-		$sql3 = "insert into 예약 values ($num, '$name',NULL, NULL)";
-		$result3 = mysqli_query($mysqli,$sql3);
+		echo("<script src=\"http://code.jquery.com/jquery-latest.min.js\"></script>");
 
-		echo "<script>location.href='res_department.html'</script>";
-
-?>
+	 echo "<script>location.href='res_department.html?num='+$num</script>";
+	?>
