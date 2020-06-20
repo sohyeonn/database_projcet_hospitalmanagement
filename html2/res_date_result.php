b29d80c38d542923150f08fde2512e28a74d6522
kimgywls <65155823+kimgywls@users.noreply.github.com>
2020-06-20 14:40:53 +0900

    Update res_date_result.php

--- html2/res_date_result.php
+++ html2/res_date_result.php
@@ -1,3 +1,18 @@
+<script>
+
+       function confirmtest(num){
+
+             var confirmflag = confirm("입원을 원하십니까??");
+           if(confirmflag){
+             location.href='res_room.html?num='+num
+
+           }else{
+             location.href='res_payment.html?num='+num
+           }
+       }
+</script>
+
+
 <?php
     $host = 'localhost';
     $user = 'root';
@@ -5,10 +20,12 @@
     $dbName = 'project';
     $mysqli = new mysqli($host, $user, $pw, $dbName);
 
-    $number=$_GET['date'];
-    $name=$_GET['name'];
     $date=$_GET['date'];
-    $doctor=$_GET['doctor'];
+    $num=$_GET['num'];
 
-    echo date;
+    $sql1 = "update 환자 set 치료날짜 = $date where 환자번호= $num";
+		$result1 = mysqli_query($mysqli,$sql1);
+
+    echo "<script>confirmtest($num)</script>";
+
 ?>
