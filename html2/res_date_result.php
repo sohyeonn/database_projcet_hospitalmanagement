bcafa6f2d45c3856b04171fe511c65ded455d248
kimgywls <65155823+kimgywls@users.noreply.github.com>
2020-06-21 22:31:53 +0900

    Update res_date_result.php

--- html2/res_date_result.php
+++ html2/res_date_result.php
@@ -1,3 +1,30 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $pw = '0000';
+    $dbName = 'project';
+    $mysqli = new mysqli($host, $user, $pw, $dbName);
+
+    $date=$_GET['date'];
+    $num=$_GET['num'];
+
+
+
+    echo $date;
+    echo $num;
+
+    $sql1 = "update 환자 set 치료날짜='$date' where 환자번호= $num";
+
+  /*  if(mysqli_query($mysqli,$sql1)) {
+      echo "okay";
+    }
+    else {
+      echo mysqli_error($mysqli);
+    }*/
+
+    echo "<script>confirmtest($num)</script>";
+
+?>
 <script>
 
        function confirmtest(num){
@@ -11,21 +38,3 @@
            }
        }
 </script>
-
-
-<?php
-    $host = 'localhost';
-    $user = 'root';
-    $pw = '0000';
-    $dbName = 'project';
-    $mysqli = new mysqli($host, $user, $pw, $dbName);
-
-    $date=$_GET['date'];
-    $num=$_GET['num'];
-
-    $sql1 = "update 환자 set 치료날짜 = $date where 환자번호= $num";
-		$result1 = mysqli_query($mysqli,$sql1);
-
-    echo "<script>confirmtest($num)</script>";
-
-?>
